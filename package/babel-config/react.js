ab1eec4017bbf1970f332b11784643ed925e0530
xunserver <1358925129@qq.com>
2022-08-22 16:43:15 +0800

    新增babel配置

---
+++ package/babel-config/react.js
@@ -0,0 +1,47 @@
+module.exports = (_, options) => {
+    const {
+      typescript = false,
+      envOptions = {},
+      reactOptions = {},
+      typescriptOptions = {},
+    } = options;
+    const presets = [
+      [
+        "@babel/preset-env",
+        {
+          corejs: 3,
+          useBuiltIns: 'usage',
+          ...envOptions,
+        },
+      ],
+      [
+        '@babel/preset-react', {
+            ...reactOptions
+        }
+      ]
+    ];
+  
+    const plugins = [
+      [
+        "@babel/plugin-transform-runtime",
+        {
+          corejs: 3,
+        },
+      ],
+    ];
+  
+    if (typescript) {
+      presets.push([
+          '@babel/preset-typescript', {
+              allExtensions: true,
+              ...typescriptOptions
+          }
+      ]);
+    }
+  
+    return {
+      presets,
+      plugins
+    }
+  };
+  

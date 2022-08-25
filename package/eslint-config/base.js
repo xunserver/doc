7c94cb58fe725060446570dfb527313853e8a70a
xunserver <1358925129@qq.com>
2022-08-25 17:50:34 +0800

    调整eslint插件

--- package/eslint-config/base.js
+++ package/eslint-config/base.js
@@ -1,8 +1,8 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
   extends: [
-    "eslint:recommended", // 官方标准规则
-    "standard", // eslint-config-standard 稍微严格的规则
-    "plugin:prettier/recommended",
+    'eslint:recommended', // 官方标准规则
+    'standard', // eslint-config-standard 稍微严格的规则
+    // "plugin:prettier/recommended",
+    'prettier', // 关闭eslint和prettier冲突的选项
   ],
-};
+}

324b0a272df0e6f7d4127fd5e486407a8b0127f6
xunserver <1358925129@qq.com>
2022-08-26 15:25:55 +0800

    123

--- package/stylelint-config/scss.js
+++ package/stylelint-config/scss.js
@@ -1,4 +1,18 @@
 module.exports = {
     extends: ['./css.js', 'stylelint-config-standard-scss', 'stylelint-config-prettier'],
-    plugins: ['stylelint-scss']
+    plugins: ['stylelint-scss'],
+    rules: {
+        'scss/at-import-no-partial-leading-underscore': [
+          true,
+          {
+            severity: 'warning',
+          },
+        ],
+        'annotation-no-unknown': [
+          true,
+          {
+            severity: 'warning',
+          },
+        ],
+    }
 }

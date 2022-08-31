48dd604cfa6352ff03119a5ce66511277912f400
xunserver <1358925129@qq.com>
2022-08-31 17:50:04 +0800

    feat(cli):[pub]新增postcss配置

--- package/cli/build/publish.js
+++ package/cli/build/publish.js
@@ -8,7 +8,7 @@ async function main() {
     shelljs.exec('npm publish --access public');
 
     // 更新git
-    shelljs.exec('git add package/cli/package.json');
+    shelljs.exec('git add package.json');
     const packageJson = require('../package.json')
     shelljs.exec(`git commit -m "${packageJson.version}"`);
     shelljs.exec(`git push origin main`);

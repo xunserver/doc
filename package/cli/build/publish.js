8809c1c8149b3c7d8c1276079b355c5c532d2bf2
xunserver <1358925129@qq.com>
2022-08-31 10:24:17 +0800

    feat(cli):[pub]新增github action

--- package/cli/build/publish.js
+++ package/cli/build/publish.js
@@ -7,15 +7,15 @@ const checkIsPub = () => {
 
 async function main() {
     // 判断是否需要pub,根据是否存在[pub]前缀决定是否发版
-    // if(!await checkIsPub()) {
-    //     return
-    // }
+    if(!await checkIsPub()) {
+        return
+    }
 
     // 版本+1
     shelljs.exec('npm version patch');
 
     // 发布npm
-    // shelljs.exec('npm publish --access public');
+    shelljs.exec('npm publish --access public');
 
     // 更新git
     shelljs.exec('git add -A');

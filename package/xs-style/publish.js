c0cdb6b2b9058f30b35b9ac7487630acf72e88d4
yangmanman <1358925129@qq.com>
2022-09-02 10:13:14 +0800

    feat(style): [pub]初始化style

---
+++ package/xs-style/publish.js
@@ -0,0 +1,19 @@
+const shelljs = require('shelljs');
+
+shelljs.config.fatal = true;
+
+async function main() {
+    // 版本+1
+    shelljs.exec('npm version patch');
+
+    // 发布npm
+    shelljs.exec('npm publish --access public');
+
+    // 更新git
+    shelljs.exec('git add package.json');
+    const packageJson = require('./package.json')
+    shelljs.exec(`git commit -m "${packageJson.version}"`);
+    shelljs.exec(`git push origin main`);
+}
+
+main()

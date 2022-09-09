d1df54f10adbff7a216f4f9c75bcb0ea26a9bf82
xunserver <1358925129@qq.com>
2022-09-09 21:18:22 +0800

    123

--- package/doc/doc/.vitepress/config.js
+++ package/doc/doc/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 export default defineConfig({
   outDir: "../dist",
+  base: process.env.APP_BASE || "/",
   themeConfig: {
     siteTitle: "xunserver",
     nav: [

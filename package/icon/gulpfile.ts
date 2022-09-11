5f9ec097873a9b60d47662cdbc2a81e2a7899190
xunserver <1358925129@qq.com>
2022-09-11 23:08:06 +0800

    123

--- package/icon/gulpfile.ts
+++ package/icon/gulpfile.ts
@@ -1,14 +1,19 @@
 import del from "del";
 import { src, dest, parallel, task, series } from "gulp";
+import { Transform } from "stream";
+import { isBuffer } from "util";
 
 export default series(
   () => del(["src", "dist"]),
   parallel(
     () => src("template/*").pipe(dest("src/template")),
     () =>
-      src("svg/*").pipe((file: File, encoding, done) => {
-        console.log(args);
-        return;
-      })
+      src("svg/**/aim.svg").pipe(
+        new Transform({
+          transform(chunk, enc, callback) {
+            return callback(null, chunk);
+          },
+        })
+      )
   )
 );

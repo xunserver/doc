9fb01969659fa49de0a8d489da3c9c3bb7d51ed9
xunserver <1358925129@qq.com>
2022-09-27 14:54:46 +0800

    feat(cli):[pub] 新增ts支持

--- package/cli/build/publish.js
+++ package/cli/build/publish.js
@@ -1,11 +1,14 @@
 const shelljs = require("shelljs");
 
 async function main() {
+  shelljs.config.fatal = true;
   // 版本+1
   shelljs.exec("npm version patch");
 
   // 发布npm
-  shelljs.exec("pnpm publish -F @xunserver/cli --access public");
+  shelljs.exec(
+    "pnpm publish -F @xunserver/cli --access public --no-git-checks"
+  );
 
   // 更新git
   shelljs.exec("git add package.json");
